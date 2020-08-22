cc84ad8a10fe847bc26fd33090b7bbb7b2fed53d
Paschalis Bizopoulos <pbizopoulos@iti.gr>
2020-08-22 13:57:17 +0300

    improve text

--- main.py
+++ main.py
@@ -20,19 +20,18 @@ plt.rcParams['savefig.format'] = 'pdf'
 class CustomModel(nn.Module):
     def __init__(self):
         super(CustomModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 4, 5, 1)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(2304, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.dropout1(x)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
         x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
