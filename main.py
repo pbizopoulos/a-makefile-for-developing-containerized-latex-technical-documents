542705cbed68b0b0ed5aa277cf4ca9037a231d55
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-03-29 00:40:44 +0300

    update main.py

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ def main():
     activation_function_list = ['ReLU', 'ReLU6', 'SiLU']
     mean_std_list = [((0.1307,), (0.3081,)), ((0.1307,), (0.3081,)), ((0.1307,), (0.3081,)), ((0.1307,), (0.3081,))]
     num_epochs = 20
-    train_range_list = [range(50000), range(50000), range(50000), range(50000)]
+    training_range_list = [range(50000), range(50000), range(50000), range(50000)]
     validation_range_list = [range(50000, 60000), range(50000, 60000), range(50000, 60000), range(50000, 60000)]
     test_range_list = [range(10000), range(10000), range(10000), range(10000)]
     if not full:
         num_epochs = 2
-        train_range_list = [train_range[:10] for train_range in train_range_list]
+        training_range_list = [training_range[:10] for training_range in training_range_list]
         validation_range_list = [validation_range[:10] for validation_range in validation_range_list]
         test_range_list = [test_range[:10] for test_range in test_range_list]
     np.random.seed(0)
@@ -49,18 +49,18 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     lr = 0.01
     batch_size = 64
-    train_loss_array = np.zeros((len(dataset_list), len(activation_function_list), num_epochs))
+    training_loss_array = np.zeros((len(dataset_list), len(activation_function_list), num_epochs))
     validation_loss_array = np.zeros((len(dataset_list), len(activation_function_list), num_epochs))
     test_accuracy_array = np.zeros((len(dataset_list), len(activation_function_list)))
     test_batch_size = 1000
     num_parameters = np.zeros(len(activation_function_list))
     criterion = nn.CrossEntropyLoss()
-    for index_dataset, (dataset, dataset_name, train_range, validation_range, test_range, mean_std) in enumerate(zip(dataset_list, dataset_name_list, train_range_list, validation_range_list, test_range_list, mean_std_list)):
+    for index_dataset, (dataset, dataset_name, training_range, validation_range, test_range, mean_std) in enumerate(zip(dataset_list, dataset_name_list, training_range_list, validation_range_list, test_range_list, mean_std_list)):
         transform = transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: torch.cat([x, x, x], 0)), transforms.Normalize(mean_std[0], mean_std[1])])
-        train_dataset = dataset(artifacts_dir, transform=transform, download=True)
+        training_dataset = dataset(artifacts_dir, transform=transform, download=True)
         test_dataset = dataset(artifacts_dir, train=False, transform=transform, download=True)
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_range))
-        validation_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(validation_range))
+        training_dataloader = DataLoader(training_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(training_range))
+        validation_dataloader = DataLoader(training_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(validation_range))
         test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, sampler=SubsetRandomSampler(test_range))
         for index_activation_function, activation_function in enumerate(activation_function_list):
             model = mobilenet_v2().to(device)
@@ -71,12 +71,12 @@ def main():
             num_parameters[index_activation_function] = sum(p.numel() for p in model.parameters() if p.requires_grad)
             optimizer = optim.SGD(model.parameters(), lr=lr)
             validation_loss_best = float('inf')
-            filename = f'{dataset.__name__}-{activation_function}'
-            model_path = join(artifacts_dir, f'{filename}.pt')
-            for index_epoch, epoch in enumerate(range(num_epochs)):
-                train_loss_sum = 0
+            model_filename = f'{dataset.__name__}-{activation_function}'
+            model_path = join(artifacts_dir, f'{model_filename}.pt')
+            for epoch in range(num_epochs):
+                training_loss_sum = 0
                 model.train()
-                for data, target in train_dataloader:
+                for data, target in training_dataloader:
                     data = data.to(device)
                     target = target.to(device)
                     optimizer.zero_grad()
@@ -84,9 +84,9 @@ def main():
                     loss = criterion(output, target)
                     loss.backward()
                     optimizer.step()
-                    train_loss_sum += loss.item()
-                train_loss = train_loss_sum / len(train_dataloader)
-                train_loss_array[index_dataset, index_activation_function, index_epoch] = train_loss
+                    training_loss_sum += loss.item()
+                training_loss = training_loss_sum / len(training_dataloader)
+                training_loss_array[index_dataset, index_activation_function, epoch] = training_loss
                 model.eval()
                 validation_loss_sum = 0
                 correct = 0
@@ -101,13 +101,13 @@ def main():
                         correct += prediction.eq(target.view_as(prediction)).sum().item()
                         total += output.shape[0]
                 validation_loss = validation_loss_sum / len(validation_dataloader)
-                validation_loss_array[index_dataset, index_activation_function, index_epoch] = validation_loss
+                validation_loss_array[index_dataset, index_activation_function, epoch] = validation_loss
                 accuracy = 100.0 * correct / total
                 print(f'{dataset_name}, {activation_function}, Epoch: {epoch}, Validation average loss: {validation_loss:.4f}, Validation accuracy: {accuracy:.2f}%')
                 if validation_loss < validation_loss_best:
                     validation_loss_best = validation_loss
                     torch.save(model.state_dict(), model_path)
-                    print('saving as best model')
+                    print('Saving as best model.')
             model = mobilenet_v2().to(device)
             if activation_function == 'SiLU':
                 change_module(model, nn.ReLU6, nn.SiLU)
@@ -116,7 +116,7 @@ def main():
             model.load_state_dict(torch.load(model_path))
             model.eval()
             kernels = model.features[0][0].weight.detach().clone()
-            save_image(kernels[:25], join(artifacts_dir, f'{filename}-kernels.pdf'), padding=1, nrow=5, normalize=True)
+            save_image(kernels[:25], join(artifacts_dir, f'{model_filename}-kernels.pdf'), padding=1, nrow=5, normalize=True)
             correct = 0
             total = 0
             with torch.no_grad():
@@ -132,7 +132,7 @@ def main():
                 print(f'{dataset_name}, {activation_function}, Test accuracy: {accuracy:.2f}%')
     df_keys_values = pd.DataFrame({'key': ['num-epochs', 'batch-size', 'lr'], 'value': [str(int(num_epochs)), str(int(batch_size)), lr]})
     df_keys_values.to_csv(join(artifacts_dir, 'keys-values.csv'))
-    for dataset_name, train_loss, validation_loss in zip(dataset_name_list, train_loss_array, validation_loss_array):
+    for dataset_name, training_loss, validation_loss in zip(dataset_name_list, training_loss_array, validation_loss_array):
         _, ax = plt.subplots()
         plt.grid(True)
         plt.autoscale(enable=True, axis='x', tight=True)
@@ -140,8 +140,8 @@ def main():
         plt.xlabel('Epochs', fontsize=18)
         if dataset_name == 'MNIST':
             plt.ylabel('loss', fontsize=18)
-        for train_loss_, validation_loss_, activation_function, color in zip(train_loss, validation_loss, activation_function_list, ['b', 'orange']):
-            plt.plot(train_loss_, label=f'Train {activation_function}', color=color)
+        for training_loss_, validation_loss_, activation_function, color in zip(training_loss, validation_loss, activation_function_list, ['b', 'orange']):
+            plt.plot(training_loss_, label=f'Training {activation_function}', color=color)
             plt.plot(validation_loss_, label=f'Validation {activation_function}', linestyle='--', color=color)
         plt.title(dataset_name)
         ax.tick_params(axis='both', which='major', labelsize='large')
