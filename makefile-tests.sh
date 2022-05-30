9f5e2889fc249d02676b048330524466b446c62f
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-05-30 18:55:23 +0300

    new shells

--- makefile-tests.sh
+++ makefile-tests.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -o errexit
 
-# Requirements: bash, dash, ksh, yash, zsh, bmake and make
+# Requirements: ash, bash, dash, ksh, posh, yash, zsh, bmake and make
 
 artifactsdir=artifacts/artifacts-test
 make_array=( bmake make )
-shell_array=( bash dash ksh yash zsh )
+shell_array=( ash bash dash ksh posh yash zsh )
 for make_element in "${make_array[@]}"
 do
 	for shell_element in "${shell_array[@]}"
 	do
 		mkdir -p ${artifactsdir}/python/
 		cp python/Makefile ${artifactsdir}/python/Makefile
-		"$make_element" -C ${artifactsdir}/python/ SHELL="$shell_element"
-		"$make_element" -C ${artifactsdir}/python/ SHELL="$shell_element" clean
+		"$make_element" -C ${artifactsdir}/python/ SHELL="$shell_element" || rm -rf ${artifactsdir}
+		"$make_element" -C ${artifactsdir}/python/ SHELL="$shell_element" clean || rm -rf ${artifactsdir}
 		cp Makefile ${artifactsdir}/Makefile
-		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element"
-		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" artifacts/tex.tar
-		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" artifacts/tex-lint
-		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" clean
+		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" || rm -rf ${artifactsdir}
+		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" artifacts/tex.tar || rm -rf ${artifactsdir}
+		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" artifacts/tex-lint || rm -rf ${artifactsdir}
+		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" artifacts/ms-server.pdf SERVER_URL=https://arxiv.org/e-print/2005.12660 || rm -rf ${artifactsdir}
+		"$make_element" -C ${artifactsdir}/ SHELL="$shell_element" clean || rm -rf ${artifactsdir}
 		rm -rf ${artifactsdir}/
 	done
 done
